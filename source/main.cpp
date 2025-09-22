bcf4d986589a284c2b9d78290e154052ac1d98c6
ArkBird451 <muhdtalha6@gmail.com>
2025-09-22 18:23:27 +0500

    Traingles rasterization

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,17 @@
 #include <cmath>
-#include <cstdlib>
-#include <ctime>
+#include <tuple>
+#include "geometry.h"
+#include "model.h"
 #include "tgaimage.h"
+
+constexpr int width  = 128;
+constexpr int height = 128;
+
+constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green   = {  0, 255,   0, 255};
+constexpr TGAColor red     = {  0,   0, 255, 255};
+constexpr TGAColor blue    = {255, 128,  64, 255};
+constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
     bool steep = std::abs(ax-bx) < std::abs(ay-by);
@@ -13,35 +23,53 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
         std::swap(ax, bx);
         std::swap(ay, by);
     }
-    float y = ay;
-    float ierror = 0;
+    int y = ay;
+    int ierror = 0;
     for (int x=ax; x<=bx; x++) {
         if (steep) // if transposed, de−transpose
             framebuffer.set(y, x, color);
         else
             framebuffer.set(x, y, color);
         ierror += 2 * std::abs(by-ay);
-        if (ierror > bx-ax) {
+        if (ierror > bx - ax) {
             y += by > ay ? 1 : -1;
-            ierror -=2 * (bx-ax);
+            ierror -= 2 * (bx-ax);
+        }
+    }
+}
+
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+    // sort the vertices, a,b,c in ascending y order (bubblesort yay!)
+    if (ay>by) { std::swap(ax, bx); std::swap(ay, by); }
+    if (ay>cy) { std::swap(ax, cx); std::swap(ay, cy); }
+    if (by>cy) { std::swap(bx, cx); std::swap(by, cy); }
+    int total_height = cy-ay;
+
+    if (ay != by) { // if the bottom half is not degenerate
+        int segment_height = by - ay;
+        for (int y=ay; y<=by; y++) { // sweep the horizontal line from ay to by
+            int x1 = ax + ((cx - ax)*(y - ay)) / total_height;
+            int x2 = ax + ((bx - ax)*(y - ay)) / segment_height;
+            for (int x=std::min(x1,x2); x<std::max(x1,x2); x++)  // draw a horizontal line
+                framebuffer.set(x, y, color);
+        }
+    }
+    if (by != cy) { // if the upper half is not degenerate
+        int segment_height = cy - by;
+        for (int y=by; y<=cy; y++) { // sweep the horizontal line from by to cy
+            int x1 = ax + ((cx - ax)*(y - ay)) / total_height;
+            int x2 = bx + ((cx - bx)*(y - by)) / segment_height;
+            for (int x=std::min(x1,x2); x<std::max(x1,x2); x++)  // draw a horizontal line
+                framebuffer.set(x, y, color);
         }
     }
 }
 
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
-
-    std::srand(std::time({}));
-    for (int i=0; i<(1<<24); i++) {
-        int ax = rand()%width, ay = rand()%height;
-        int bx = rand()%width, by = rand()%height;
-
-        line(ax, ay, bx, by, framebuffer, { static_cast<std::uint8_t>(rand() % 255), static_cast<std::uint8_t>(rand() % 255), static_cast<std::uint8_t>(rand() % 255), static_cast<std::uint8_t>(rand() % 255) });
-    }
-
+    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
-
 }
